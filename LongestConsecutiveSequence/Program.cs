b87c6f66b420dd49a995b8c0dd17f4ed5b8450ab
fred <domaine75@gmail.com>
2019-09-17 11:19:20 +0200

    Initial commit

---
+++ LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LongestConsecutiveSequence
+{
+  class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("Longest consecutive Sequence");
+      int[] array1 = new int[55];
+      array1 = GenerateRandomNumbers(array1.Length);
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static int[] GenerateRandomNumbers(int length)
+    {
+      var rnd = new Random();
+      int[] result = new int[length];
+      for (int i = 0; i < length; i++)
+      {
+        result[i] = rnd.Next(999);
+      }
+
+      return result;
+    }
+  }
+}
